aba1ed5a284e78e14eb9e810619bfba6725c56d9
lucky <takuto.yoshikai@gmail.com>
2022-08-28 17:20:14 +0900

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const Transliterator = require("honja");
+const honja = new Transliterator();
+const express = require("express");
+const app = express();
+
+app.get("/All/:text", (req, res) => {
+  const text = req.params.text;
+  const result = honja.convertAll(text);
+  
+  res.status(200).json(result);
+});
+
+app.get("/:lang/:text", (req, res) => {
+  const lang = req.params.lang;
+  const text = req.params.text;
+  const result = honja.convert(text, lang);
+  
+  res.status(200).json({
+    result
+  });
+});
+
+
+app.listen(3000);
